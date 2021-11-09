4e92eefcfe0c734511898fe6306545c3c90e28b0
Hiroshi Nobuoka <hiroshi.nobuoka@gmail.com>
2021-11-09 11:21:27 -0500

    create user

--- s-collections/usersCollection.js
+++ s-collections/usersCollection.js
@@ -20,7 +20,7 @@ module.exports = {
                     message: 'usersCollection: invalid id'
                 }
             }
-            //Query users collection
+            //Query users collection. Awaited to prevent false positive due to premature return
             const user = await UserModel.findOne({_id})
 
             return {
@@ -34,10 +34,80 @@ module.exports = {
                 status: false,
                 message: `usersCollection: Error--> ${err}`
             }
-
         }
+    },
+    // [CREATE] User //
+    c_register: async (username, email, password) => {
+        try { 
+            // [VALIDATE] username //
+            if(!validator.isAscii(username)){
+                return {
+                    execution: true,
+                    status: false,
+                    message: 'UserCollection: Invalid username'
+                }
+            }
+            // [VALIDATE] email //
+            if(!validator.isEmail(email)){
+                return {
+                    execution: true,
+                    status: false,
+                    message: 'UserCollection: Invalid email'
+                }
+            }
+            // [VALIDATE] password //
+            if (!validator.isAscii(password)) {
+				return {
+					executed: true,
+					status: false,
+					message: 'UserCollection: Invalid password'
+				}
+			}
+            // Check availability username //
+            if(await UserModel.findOne( { username })) {
+                return {
+                    executed: true,
+                    status: true,
+                    message: 'That username is taken',
+                    created: false
+                }
+            }
+            // Check availability email //
+            if(await UserModel.findOne( { email })) {
+                return {
+                    executed: true,
+                    status: true,
+                    message: 'That username is taken',
+                    created: false
+                }
+            }
+            // [ENCRYPT] password //
+            const hashedPassword = await bcyrpt.hash(password, 10);
+            // [SAVE] user //
+            const user = await new UserModel({
+                _id: mongoose.Types.ObjectId,
+                username,
+                email,
+                password: hashedPassword
+            }).save();
 
+            return {
+                execution: true,
+                status: true,
+                message: 'userCollections: user created!',
+                created: true,
+                user: user
+            }
+        } catch (err) {
+            return {
+                execution: false,
+                status: false,
+                message: `userCollections Error --> ${err}`
+            }
+        }
     }
+
+
     
 
 }
